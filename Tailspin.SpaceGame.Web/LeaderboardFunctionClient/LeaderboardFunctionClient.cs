072241330b12f72446d03fc837db28c6f35f7b94
Eduardo Santiago <eduardo.santiago1987@gmail.com>
2021-09-03 19:33:22 -0500

    Bug Fix

--- Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
+++ Tailspin.SpaceGame.Web/LeaderboardFunctionClient/LeaderboardFunctionClient.cs
@@ -22,6 +22,8 @@ namespace TailSpin.SpaceGame.Web
             {
                 string json = await webClient.DownloadStringTaskAsync($"{this._functionUrl}?page={page}&pageSize={pageSize}&mode={mode}&region={region}");
                 return JsonSerializer.Deserialize<LeaderboardResponse>(json);
+                var leaderboardResponse = JsonSerializer.Deserialize<LeaderboardResponse>(json, new JsonSerializerOptions { IncludeFields = true });
+                return leaderboardResponse;
             }
         }
     }
